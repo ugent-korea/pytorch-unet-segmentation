4afe9702684ecbd4c66920a0f44702c6f14c0e5a
mijeongl <mijeong.lee@ghent.ac.kr>
2018-07-16 14:38:50 +0900

    augmentation flip

--- src/augmentation.py
+++ src/augmentation.py
@@ -29,8 +29,20 @@ def elastic_transform(image, alpha, sigma, random_state=None):
 # σ is  the  elasticity  coefficient.
 
 
-def flip(image, horizontal=False, vertical=False):
-    pass
+def flip(image, option_value):
+    if option_value = 0:
+        # vertical
+        image = np.flip(image, option_value)
+    elif option_value = 1:
+        # horizontal
+        image = np.flip(image, option_value)
+    elif option_value = 2:
+        # horizontally and vertically flip
+        image = np.flip(image, 0)
+        image = np.flip(image, 1)
+    else:
+        # no effect
+        pass
 
 
 def Gaussian_noise(image, mean, standard_deviation):
