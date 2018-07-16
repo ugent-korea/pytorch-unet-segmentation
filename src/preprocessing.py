dc3fde0489ad5334f154a201a18a1947d0856b03
juhyung <jhlee9702@hotmail.com>
2018-07-16 16:00:32 +0900

    Added comments in the file: 	preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -11,18 +11,20 @@ def Mean(img_path):
     sum = 0  # sum of all array elements from every image
 
     for image in all_image_path:
+
         image_opened = Image.open(image)
-        total_pixels = image_opened.size[0] * image_opened.size[1]
+
         # get the total number of pixels
-        img_as_array = np.asarray(image_opened)
+        total_pixels = image_opened.size[0] * image_opened.size[1]
         # Convert opened image into array
+        img_as_array = np.asarray(image_opened)
 
         img_as_array = img_as_array / 255
         img_as_array = img_as_array.sum()
         img_as_array = img_as_array / total_pixels
         sum += img_as_array
-        print(sum)
 
+    # Divide the sum of all values by the number of values present
     mean = sum / num_images
 
     return mean
@@ -32,15 +34,16 @@ def StandardDeviation(img_path):
     # Initiation
     all_image_path = glob.glob(img_path)
     num_images = len(all_image_path)  # number of images present in the path.
-    mean_value = Mean(img_path)
+    mean_value = Mean(img_path)  # Recall mean value from function above: def Mean(path)
 
     square_sum = 0  # sum of (ith element - mean)^2
 
     for image in all_image_path:
 
         image_opened = Image.open(image)
-        total_pixels = image_opened.size[0] * image_opened.size[1]
+
         # total number of pixels by multiplying width and depth of the image
+        total_pixels = image_opened.size[0] * image_opened.size[1]
         img_as_array = np.asarray(image_opened)
 
         img_as_array = img_as_array / 255
@@ -50,6 +53,7 @@ def StandardDeviation(img_path):
         ith_square = (img_as_array - mean_value) ** 2
         square_sum += ith_square
 
+    # Finishing the Standard Deviation formula
     stdev = square_sum / num_images
     stdev = (stdev) ** 0.5
 
