caf0d962565adc3acfedf8cf83686824d6015b5f
PyeongKim <pyeongeun.kim@ghent.ac.kr>
2018-07-17 20:27:15 +0900

    augmentation.py separated crop and pad function.
    dataset.py
    changed variable names to minimize memory usage

--- src/augmentation.py
+++ src/augmentation.py
@@ -114,7 +114,7 @@ def ceil_floor_image(image):
     return image
 
 
-def zero_255_image(image):
+def approximate_image(image):
     """
     Args:
         image : numpy array of image in datatype int16
@@ -167,23 +167,32 @@ def crop_pad_test(image, in_size=572, out_size=388):
     return stacked_img
 
 
-def crop_pad_train(image, in_size=572, out_size=388):
+def cropping(image, y, x):
     """crop the image and pad it to in_size
+    Args :
+        images : numpy array of images
+        y(int) : vertical location of crop
+        x(int) : horizontal location of crop
+    Return :
+        cropped_img: numpy array of cropped image
+    """
+    cropped_img = image[y:y+out_size, x:x+out_size]
+    return cropped_img
+
+
+def add_padding(image, in_size. out_size, mode):
+    """Pad the image to in_size
     Args :
         images : numpy array of images
         in_size(int) : the input_size of model
         out_size(int) : the output_size of model
+        mode(str) : mode of padding
     Return :
-        padded_img : numpy array of cropped and padded image
-        y_loc(int) : vertical location of crop
-        x_loc(int) : horizontal location of crop
+        padded_img: numpy array of padded image
     """
-    img_height, img_width = image.shape[0], image.shape[1]
-    y_loc, x_loc = randint(0, img_height-out_size), randint(0, img_width-out_size)
-    cropped_img = image[y_loc:y_loc+out_size, x_loc:x_loc+out_size]
     pad_size = int((in_size - out_size)/2)
-    padded_img = np.pad(cropped_img, pad_size, mode="symmetric")
-    return padded_img, y_loc, x_loc
+    padded_img = np.pad(cropped_img, pad_size, mode=mode)
+    return padded_img
 
 
 if __name__ == "__main__":
--- src/dataset.py
+++ src/dataset.py
@@ -24,13 +24,12 @@ class SEMDataTrain(Dataset):
             option (str): decide which dataset to import
         """
         # all file names
-        self.mask_arr = glob.glob(str(mask_path) + str("/*"))
+        self.mask_arr = glob.glob(str(mask_path) + "/*")
         self.image_arr = glob.glob(str(image_path) + str("/*"))
         self.in_size, self.out_size = in_size, out_size
         # Calculate len
         self.data_len = len(self.mask_arr)
         # calculate mean and stdev
-        self.img_mean, self.img_std = find_mean(image_path), find_stdev(image_path)
 
     def __getitem__(self, index):
         """Get specific data corresponding to the index
@@ -40,19 +39,6 @@ class SEMDataTrain(Dataset):
         Returns:
             Tensor: specific data on index which is converted to Tensor
         """
-        # Other approach using torchvision
-        """ Other approach with torchvision
-        single_image_name = self.image_arr[index]
-        img_as_img = Image.open(single_image_name)
-        img_as_np = np.asarray(img_as_img).reshape(1, 512, 512)
-        # If there is an operation
-        if self.trans == True:
-            pass
-        # Transform image to tensor
-        elif self.trans != True:
-            img_as_tensor = self.to_tensor(img_as_np)
-        """
-
         """
         # GET IMAGE
         """
@@ -64,47 +50,46 @@ class SEMDataTrain(Dataset):
         # Augmentation
         # flip {0: vertical, 1: horizontal, 2: both, 3: none}
         flip_num = 3  # randint(0, 3)
-        flip_img = flip(img_as_np, flip_num)
+        img_as_np = flip(img_as_np, flip_num)
 
-        # Noise Determine {0: Gaussian_noise, 1: uniform_noise}
-        noise_det = randint(0, 1)
-        if noise_det == 0:
+        # Noise Determine {0: Gaussian_noise, 1: uniform_noise
+        if randint(0, 1):
             # Gaussian_noise
             gaus_sd, gaus_mean = randint(0, 20), 0
-            noise_img = add_gaussian_noise(flip_img, gaus_mean, gaus_sd)
+            img_as_np = add_gaussian_noise(img_as_np, gaus_mean, gaus_sd)
         else:
             # uniform_noise
             l_bound, u_bound = randint(-20, 0), randint(0, 20)
-            noise_img = add_uniform_noise(flip_img, l_bound, u_bound)
+            img_as_np = add_uniform_noise(img_as_np, l_bound, u_bound)
 
         # Brightness
         pix_add = randint(-20, 20)
-        bright_img = change_brightness(noise_img, pix_add)
+        img_as_np = change_brightness(img_as_np, pix_add)
 
         # Elastic distort {0: distort, 1:no distort}
         distort_det = randint(0, 1)
         if distort_det == 0:
             # sigma = 4, alpha = 34
-            aug_img, seed = add_elastic_transform(bright_img, alpha=34, sigma=4)
-        else:
-            aug_img = bright_img
+            img_as_np, seed = add_elastic_transform(img_as_np, alpha=34, sigma=4)
 
-        # Crop and pad the image
-        cropped_img, y_loc, x_loc = crop_pad_train(aug_img,
-                                                   in_size=self.in_size,
-                                                   out_size=self.out_size)
+        # Crop the image
+        img_height, img_width = img_as_np.shape[0], img_as_np.shape[1]
+        y_loc, x_loc = randint(0, img_height-self.out_size), randint(0, img_width-self.out_size)
+        img_as_np = cropping(img_as_np, y=y_loc, x=x_loc)
+
+        # Pad the image
+        img_as_np = add_padding(img_as_np, in_size=self.in_size,
+                                out_size=self.out_size, mode="symmetric")
         """
         # Sanity Check for Cropped image
-        img = Image.fromarray(cropped_img)
+        img = Image.fromarray(img_as_np)
         img.show()
         print(flip_num, noise_det, distort_det, pix_add, y_loc, x_loc)
         """
 
         # Normalize the image
-        norm_img = normalize(cropped_img,
-                             mean=self.img_mean,
-                             std=self.img_std)
-        img_as_np = np.expand_dims(norm_img, axis=0)  # add additional dimension
+        img_as_np = normalize(img_as_np, mean=Training_MEAN, std=Training_STDEV)
+        img_as_np = np.expand_dims(img_as_np, axis=0)  # add additional dimension
         img_as_tensor = torch.from_numpy(img_as_np).float()  # Convert numpy array to tensor
 
         """
@@ -116,28 +101,26 @@ class SEMDataTrain(Dataset):
         msk_as_np = np.asarray(msk_as_img)
 
         # flip the mask with respect to image
-        flip_msk = flip(msk_as_np, flip_num)
+        msk_as_np = flip(msk_as_np, flip_num)
 
         # elastic_transform of mask with respect to image
         if distort_det == 0:
             # sigma = 4, alpha = 34, seed = from image transformation
-            aug_msk, _ = add_elastic_transform(flip_msk, alpha=34, sigma=4, seed=seed)
-            aug_msk = zero_255_image(aug_msk)  # images only with 0 and 255
-        else:
-            aug_msk = flip_msk
+            msk_as_np, _ = add_elastic_transform(msk_as_np, alpha=34, sigma=4, seed=seed)
+            msk_as_np = approximate_image(msk_as_np)  # images only with 0 and 255
 
         # Crop the mask
-        cropped_msk = aug_msk[y_loc:y_loc+self.out_size, x_loc:x_loc+self.out_size]
+        msk_as_np = msk_as_np[y_loc:y_loc+self.out_size, x_loc:x_loc+self.out_size]
 
         """
         # Sanity Check for mask
-        img2 = Image.fromarray(cropped_msk)
+        img2 = Image.fromarray(msk_as_np)
         img2.show()
         """
 
         # Normalize mask to only 0 and 1
-        cropped_msk = cropped_msk/255
-        msk_as_np = np.expand_dims(cropped_msk/255, axis=0)  # add additional dimension
+        msk_as_np = msk_as_np/255
+        msk_as_np = np.expand_dims(msk_as_np/255, axis=0)  # add additional dimension
         msk_as_tensor = torch.from_numpy(msk_as_np).float()  # Convert numpy array to tensor
 
         return (img_as_tensor, msk_as_tensor)
