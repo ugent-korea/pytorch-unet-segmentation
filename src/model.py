f6abc8b6f1faef8211fdbeca07bd08caaac87c2b
juhyung <jhlee9702@hotmail.com>
2018-07-18 17:19:34 +0900

    I made long laborous version of the Unet model implification

---
+++ src/model.py
@@ -0,0 +1,216 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+from PIL import Image
+from torch.nn.functional import sigmoid
+
+
+class CleanU_Net(nn.Module):
+
+    def __init__(self):
+
+        super(CleanU_Net, self).__init__()
+
+        # Conv block 1 - Down 1
+        self.conv1_block = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+        self.max1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Conv block 2 - Down 2
+        self.conv2_block = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+        self.max2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Conv block 3 - Down 3
+        self.conv3_block = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+        self.max3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Conv block 4 - Down 4
+        self.conv4_block = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+        self.max4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Conv block 5 - Down 5
+        self.conv5_block = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+
+        # Up 1
+        self.up_1 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
+
+        # Up Conv block 1
+        self.conv_up_1 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=256,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+
+        # Up 2
+        self.up_2 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
+
+        # Up Conv block 2
+        self.conv_up_2 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=128,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+
+        # Up 3
+        self.up_3 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+
+        # Up Conv block 3
+        self.conv_up_3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=64,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+
+        # Up 4
+        self.up_4 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=2, stride=2)
+
+        # Up Conv block 4
+        self.conv_up_4 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=32,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32,
+                                      kernel_size=3, padding=0, stride=1),
+            nn.ReLU(inplace=True),
+        )
+
+        # Final output
+        self.conv_final = nn.Conv2d(in_channels=32, out_channels=1,
+                                    kernel_size=3, padding=0, stride=1)
+
+    def forward(self, x):
+        print('input', x.shape)
+        # Down 1
+        x = self.conv1_block(x)
+        print('after conv1', x.shape)
+        conv1_out = x  # Save out1
+        conv1_dim = x.shape[2]
+        x = self.max1(x)
+        print('before conv2', x.shape)
+        # Down 2
+        x = self.conv2_block(x)
+        print('after conv2', x.shape)
+        conv2_out = x
+        conv2_dim = x.shape[2]
+        x = self.max2(x)
+        print('before conv3', x.shape)
+        # Down 3
+        x = self.conv3_block(x)
+        print('after conv3', x.shape)
+        conv3_out = x
+        conv3_dim = x.shape[2]
+        x = self.max3(x)
+        print('before conv4', x.shape)
+        # Down 4
+        x = self.conv4_block(x)
+        print('after conv5', x.shape)
+        conv4_out = x
+        conv4_dim = x.shape[2]
+        x = self.max4(x)
+
+        # Midpoint
+        x = self.conv5_block(x)
+
+
+        # Up 1
+        x = self.up_1(x)
+        print('up_1', x.shape)
+        lower = int((conv4_dim - x.shape[2]) / 2)
+        upper = int(conv4_dim - lower)
+        conv4_out_modified = conv4_out[:, :, lower:upper, lower:upper]
+        x = torch.cat([x, conv4_out_modified], dim=1)
+        print('after cat_1', x.shape)
+        x = self.conv_up_1(x)
+        print('after conv_1', x.shape)
+
+        # Up 2
+        x = self.up_2(x)
+        print('up_2', x.shape)
+        lower = int((conv3_dim - x.shape[2]) / 2)
+        upper = int(conv3_dim - lower)
+        conv3_out_modified = conv3_out[:, :, lower:upper, lower:upper]
+        x = torch.cat([x, conv3_out_modified], dim=1)
+        print('after cat_2', x.shape)
+        x = self.conv_up_2(x)
+        print('after conv_2', x.shape)
+
+        # Up 3
+        x = self.up_3(x)
+        print('up_3', x.shape)
+        lower = int((conv2_dim - x.shape[2]) / 2)
+        upper = int(conv2_dim - lower)
+        conv2_out_modified = conv2_out[:, :, lower:upper, lower:upper]
+        x = torch.cat([x, conv2_out_modified], dim=1)
+        print('after cat_3', x.shape)
+        x = self.conv_up_3(x)
+        print('after conv_3', x.shape)
+        # Up 4
+        x = self.up_4(x)
+        print('up_4', x.shape)
+        lower = int((conv1_dim - x.shape[2]) / 2)
+        upper = int(conv1_dim - lower)
+        conv1_out_modified = conv1_out[:, :, lower:upper, lower:upper]
+        x = torch.cat([x, conv1_out_modified], dim=1)
+        print('after cat_4', x.shape)
+        x = self.conv_up_4(x)
+        print('after conv_4', x.shape)
+
+        # Final output
+        x = self.conv_final(x)
+
+        return x
+
+
+if __name__ == "__main__":
+    # A full forward pass
+    im = torch.randn(1, 1, 572, 572)
+    model = CleanU_Net()
+    x = model(im)
+    #print(x.shape)
+    del model
+    del x
+    # print(x.shape)
