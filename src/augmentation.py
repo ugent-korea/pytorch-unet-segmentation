635654ecd61520053021029a8b2bbdd5d2333073
PyeongKim <pyeongeun.kim@ghent.ac.kr>
2018-07-18 11:24:13 +0900

    augmentation.py - separated task of finding stride size to stride_size function - applied changed setting to all codes that contains stride_size

--- src/augmentation.py
+++ src/augmentation.py
@@ -142,28 +142,42 @@ def normalize(image, mean, std):
     return image
 
 
-def multi_cropping(image, crop_size, dim1_num, dim2_num):
+def stride_size(image_len, crop_num, crop_size):
+    """return stride size
+    Args :
+        image_len(int) : length of one size of image (width or height)
+        crop_num(int) : number of crop in certain direction
+        crop_size(int) : size of crop
+    Return :
+        stride_size(int) : stride size
+    """
+    return int((image_len - crop_size)/(crop_num - 1))
+
+
+def multi_cropping(image, crop_size, crop_num1, crop_num2):
     """crop the image and pad it to in_size
     Args :
         images : numpy arrays of images
         crop_size(int) : size of cropped image
-        dim2_num (int) : number of crop in horizontal way
-        dim1_num (int) : number of crop in vertical way
+        crop_num2 (int) : number of crop in horizontal way
+        crop_num1 (int) : number of crop in vertical way
     Return :
         cropped_imgs : numpy arrays of stacked images
     """
 
     img_height, img_width = image.shape[0], image.shape[1]
-    assert crop_size*dim1_num >= img_width and crop_size * \
-        dim2_num >= img_height, "Whole image cannot be sufficiently expressed"
-    assert dim1_num <= img_width - crop_size + 1 and dim2_num <= img_height - \
+    assert crop_size*crop_num1 >= img_width and crop_size * \
+        crop_num2 >= img_height, "Whole image cannot be sufficiently expressed"
+    assert crop_num1 <= img_width - crop_size + 1 and crop_num2 <= img_height - \
         crop_size + 1, "Too many number of crops"
 
     cropped_imgs = []
-    dim1_stride = int((img_height - crop_size)/(dim1_num - 1))
-    dim2_stride = int((img_width - crop_size)/(dim2_num - 1))
-    for i in range(dim1_num):
-        for j in range(dim2_num):
+    # int((img_height - crop_size)/(crop_num1 - 1))
+    dim1_stride = stride_size(img_height, crop_num1, crop_size)
+    # int((img_width - crop_size)/(crop_num2 - 1))
+    dim2_stride = stride_size(img_width, crop_num2, crop_size)
+    for i in range(crop_num1):
+        for j in range(crop_num2):
             cropped_imgs.append(cropping(image, crop_size,
                                          dim1_stride*i, dim2_stride*j))
     return np.asarray(cropped_imgs)
@@ -215,30 +229,52 @@ def add_padding(image, in_size, out_size, mode):
     return padded_img
 
 
-def division_array(image, dim1_num, dim2_num, img_dim1, img_dim2):
-    crop_size = image.shape[1]
-    div_array = np.zeros([img_dim1, img_dim2])
-    one_array = np.ones([crop_size, crop_size])
-    dim1_stride = int((img_dim1 - crop_size)/(dim1_num - 1))
-    dim2_stride = int((img_dim2 - crop_size)/(dim2_num - 1))
-    for i in range(dim1_num):
-        for j in range(dim2_num):
+def division_array(crop_size, crop_num1, crop_num2, dim1, dim2):
+    """Make division array
+    Args :
+        crop_size(int) : size of cropped image
+        crop_num2 (int) : number of crop in horizontal way
+        crop_num1 (int) : number of crop in vertical way
+        dim1(int) : vertical size of output
+        dim2(int) : horizontal size_of_output
+    Return :
+        div_array : numpy array of numbers of 1,2,4
+    """
+    div_array = np.zeros([dim1, dim2])  # make division array
+    one_array = np.ones([crop_size, crop_size])  # one array to be added to div_array
+    dim1_stride = stride_size(dim1, crop_num1, crop_size)  # vertical stride
+    dim2_stride = stride_size(dim2, crop_num2, crop_size)  # horizontal stride
+    for i in range(crop_num1):
+        for j in range(crop_num2):
+            # add ones to div_array at specific position
             div_array[dim1_stride*i:dim1_stride*i + crop_size,
                       dim2_stride*j:dim2_stride*j + crop_size] += one_array
     return div_array
 
 
-def prediction(image, dim1_num, dim2_num, img_dim1, img_dim2):
-    crop_size = image.shape[1]
-    div_array = np.zeros([img_dim1, img_dim2])
-    dim1_stride = int((img_dim1 - crop_size)/(dim1_num - 1))
-    dim2_stride = int((img_dim2 - crop_size)/(dim2_num - 1))
-    for i in range(dim1_num):
-        for j in range(dim2_num):
-            div_array[dim1_stride*i:dim1_stride*i + crop_size,
-                      dim2_stride*j:dim2_stride*j + crop_size] += one_array
-    return div_array
-    return None
+def image_concatenate(image, crop_num1, crop_num2, dim1, dim2):
+    """concatenate images
+    Args :
+        image : output images (should be square)
+        crop_num2 (int) : number of crop in horizontal way (2)
+        crop_num1 (int) : number of crop in vertical way (2)
+        dim1(int) : vertical size of output (512)
+        dim2(int) : horizontal size_of_output (512)
+    Return :
+        div_array : numpy arrays of numbers of 1,2,4
+    """
+    crop_size = image.shape[1]  # size of crop
+    empty_array = np.zeros([dim1, dim2]).astype("int16")  # to make sure no overflow
+    dim1_stride = stride_size(dim1, crop_num1, crop_size)  # vertical stride
+    dim2_stride = stride_size(dim2, crop_num2, crop_size)  # horizontal stride
+    index = 0
+    for i in range(crop_num1):
+        for j in range(crop_num2):
+            # add image to empty_array at specific position
+            empty_array[dim1_stride*i:dim1_stride*i + crop_size,
+                        dim2_stride*j:dim2_stride*j + crop_size] += image[index]
+            index += 1
+    return empty_array
 
 
 if __name__ == "__main__":
@@ -247,7 +283,10 @@ if __name__ == "__main__":
     a.show()
     a = np.array(a)
     croped = multi_cropping(a, 388, 2, 2)
-    div_arr = division_array(croped, 2, 2, 512, 512)*60
-    a_11 = Image.fromarray(div_arr)
+    div_arr = division_array(388, 2, 2, 512, 512)
+    cont_arr = image_concatenate(croped, 2, 2, 512, 512)
+    a_11 = Image.fromarray(div_arr*60)
+    a_22 = Image.fromarray(cont_arr/div_arr)
 
     a_11.show()
+    a_22.show()
