14df1fb9a578db54ef4aca34b79604647b0dab9a
juhyung <jhlee9702@hotmail.com>
2018-07-23 16:58:06 +0900

    completed script for plotting graph (epoch vs loss)

--- src/epoch_loss_plot.py
+++ src/epoch_loss_plot.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
+import numpy as np
 
 '''
 For members who did not yet install the module "matplotlib",
@@ -19,8 +20,43 @@ if you have python version >= 3.6,
 Now you won't have problems running this python file.
 '''
 
-loss_values = pd.read_csv('train_loss.csv')
-#SANITY CHECK
-print(loss_values.head)
+def plotloss(csvfile):
 
-loss_values.plot()
+    '''
+    Args
+        csvfile: name of the csv file
+
+    Returns
+        graph_loss: trend of loss values over epoch
+    '''
+
+    # Bring in the csv file
+    loss_values = pd.read_csv(csvfile)
+
+    # Initiation
+    epoch = loss_values.iloc[:,0]
+    tr_loss = loss_values.iloc[:,1]
+    val_loss = np.asarray(loss_values.iloc[:,2])
+
+    # Reduce the volume of data
+    epoch_skip = epoch[::10]
+    tr_loss_skip = tr_loss[::10]
+    val_loss_skip = val_loss[::10]
+
+    train_loss = plt.plot(epoch_skip, tr_loss_skip, 'b-', label='Train loss')
+    valid_loss = plt.plot(epoch_skip, val_loss_skip, 'g-', label='Validation loss')
+
+    # Red line to indicate the optimal epoch (absolute value)
+    minimum = np.argmin(val_loss)
+    optimal = plt.axvline(x=minimum, color='r', label='Optimal epoch')
+
+    plt.legend()
+
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+
+    return train_loss, valid_loss
+
+if __name__ == '__main__':
+    file = 'progress_report.csv'
+    plt.show(plotloss(file))
