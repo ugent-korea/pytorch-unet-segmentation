a77159a03739822adda1f28ef583d90b95d1656f
mijeongl <mijeong.lee@ghent.ac.kr>
2018-07-24 19:08:30 +0900

    name change

--- src/save_history.py
+++ src/save_history.py
@@ -1,19 +1,19 @@
 import os
 import csv
 
-def export_history(header,value,folder,file) :
+def export_history(header,value,folder,file_name) :
     if not os.path.exists(folder) :
         os.makedirs(folder)
 
-    file_existence = os.path.isfile(file)
+    file_existence = os.path.isfile(file_name)
     if file_existence == False :
-        file = open(file,'w',newline='')
-        writer = csv.writer(file)
+        file = open(file_name,'w',newline='')
+        writer = csv.writer(file_name)
         writer.writerow(header)
         writer.writerow(value)
     else :
-        file = open(file,'a',newline='')
-        writer = csv.writer(file)
+        file = open(file_name,'a',newline='')
+        writer = csv.writer(file_name)
         writer.writerow(value)
 
     file.close()
