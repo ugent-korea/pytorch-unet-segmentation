7ceb09698ee3d36064723b051c180c5db6e8070c
PyeongKim <pyeongeun.kim@ghent.ac.kr>
2018-07-24 11:48:01 +0900

    removed reverse

--- src/dataset.py
+++ src/dataset.py
@@ -112,10 +112,8 @@ class SEMDataTrain(Dataset):
 
         # Normalize mask to only 0 and 1
         msk_as_np = msk_as_np/255
-        msk_as_np_reverse = np.abs(msk_as_np - 1)
-        msk_two_channels = np.asarray([msk_as_np, msk_as_np_reverse])
         # msk_as_np = np.expand_dims(msk_as_np, axis=0)  # add additional dimension
-        msk_as_tensor = torch.from_numpy(msk_two_channels).long()  # Convert numpy array to tensor
+        msk_as_tensor = torch.from_numpy(msk_as_np).long()  # Convert numpy array to tensor
 
         return (img_as_tensor, msk_as_tensor)
 
@@ -197,10 +195,9 @@ class SEMDataVal(Dataset):
                                    crop_num1=2, crop_num2=2)
 
         msk_as_np = msk_as_np/255
-        msk_as_np_reverse = np.abs(msk_as_np - 1)
-        msk_two_channels = np.asarray([msk_as_np, msk_as_np_reverse])
+
         # msk_as_np = np.expand_dims(msk_as_np, axis=0)  # add additional dimension
-        msk_as_tensor = torch.from_numpy(msk_two_channels).long()  # Convert numpy array to tensor
+        msk_as_tensor = torch.from_numpy(msk_as_np).long()  # Convert numpy array to tensor
 
         return (img_as_tensor, msk_as_tensor)
 
