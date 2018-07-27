9431c2d7d8e0032a9bd694e362be5c0e8fdde45f
PyeongKim <pyeongeun.kim@ghent.ac.kr>
2018-07-27 14:50:10 +0900

    changed names

--- src/result_visualization.py
+++ src/result_visualization.py
@@ -16,8 +16,8 @@ installation of tkinter is a prerequisite. If you do not have it,
 Now you won't have problems running this python file.
 '''
 
-def plotloss(csvfile):
 
+def plotloss(csvfile):
     '''
     Args
         csvfile: name of the csv file
@@ -30,11 +30,11 @@ def plotloss(csvfile):
     loss_values = pd.read_csv(csvfile)
 
     # Initiation
-    epoch = loss_values.iloc[:,0]
-    tr_loss = loss_values.iloc[:,1]
-    tr_acc = loss_values.iloc[:,2]
-    val_loss = np.asarray(loss_values.iloc[:,3])
-    val_acc = np.asarray(loss_values.iloc[:,4])
+    epoch = loss_values.iloc[:, 0]
+    tr_loss = loss_values.iloc[:, 1]
+    tr_acc = loss_values.iloc[:, 2]
+    val_loss = np.asarray(loss_values.iloc[:, 3])
+    val_acc = np.asarray(loss_values.iloc[:, 4])
 
     # Reduce the volume of data
     epoch_skip = epoch[::20]
@@ -44,23 +44,25 @@ def plotloss(csvfile):
     val_acc_skip = val_acc[::20]
 
     # Label loss graph with dash
-    train_loss = plt.plot(epoch_skip, tr_loss_skip, linewidth=2, ls='--', color='#0059ba', label='Train loss')
-    valid_loss = plt.plot(epoch_skip, val_loss_skip, linewidth=2, ls='--', color='#47ba00', label='Validation loss')
+    train_loss = plt.plot(epoch_skip, tr_loss_skip, linewidth=2,
+                          ls='--', color='#0059ba', label='Train loss')
+    valid_loss = plt.plot(epoch_skip, val_loss_skip, linewidth=2, ls='--',
+                          color='#47ba00', label='Validation loss')
     # Label acc graph with regular line
-    train_acc = plt.plot(epoch_skip, tr_acc_skip, linewidth=2, color='#0059ba', label='Train Accuracy')
-    valid_acc = plt.plot(epoch_skip, val_acc_skip, linewidth=2, color='#47ba00', label='Validation Accuracy')
+    train_acc = plt.plot(epoch_skip, tr_acc_skip, linewidth=2,
+                         color='#0059ba', label='Train Accuracy')
+    valid_acc = plt.plot(epoch_skip, val_acc_skip, linewidth=2,
+                         color='#47ba00', label='Validation Accuracy')
 
     # Red line to indicate the optimal epoch (absolute value)
-    minimum = np.argmin(val_loss)
-    optimal = plt.axvline(x=minimum, color='r', label='Optimal epoch')
 
     plt.legend()
     plt.xlim(xmin=0)
     plt.xlabel('epochs')
 
-
     return train_loss, valid_loss
 
+
 if __name__ == '__main__':
-    file = 'history_SGD.csv'
+    file = '../history/RMS/history_RMS.csv'
     plt.show(plotloss(file))
