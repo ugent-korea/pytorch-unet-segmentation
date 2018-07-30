f4affc9f69aa4731cb7adf875573c440978f4216
juhyung <jhlee9702@hotmail.com>
2018-07-30 12:10:31 +0900

    updated the code for result+visualization.py

--- src/result_visualization.py
+++ src/result_visualization.py
@@ -41,28 +41,46 @@ def plotloss(csvfile):
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
-    # Label loss graph with dash
-    ax1.set_xlabel('Epoch', fontsize=13)
-    ax1.set_ylabel('Loss', fontsize=13)
-    ax2.set_ylabel('Accuracy', fontsize=13)
+    # Label and color the axes
+    ax1.set_xlabel('Epoch', fontsize=16)
+    ax1.set_ylabel('Loss', fontsize=16, color='black')
+    ax2.set_ylabel('Accuracy', fontsize=16, color='black')
 
+    # Plot valid/train losses
     ax1.plot(epoch_skip, tr_loss_skip, linewidth=2,
-             ls='--', color='#0059ba', label='Train loss')
-    ax1.plot(epoch_skip, val_loss_skip, linewidth=2, ls='--',
-             color='#47ba00', label='Validation loss')
-    # Label acc graph with regular line
-    ax2.plot(epoch_skip, tr_acc_skip, linewidth=2,
-             color='#0059ba', label='Train Accuracy')
-    ax2.plot(epoch_skip, val_acc_skip, linewidth=2,
-             color='#47ba00', label='Validation Accuracy')
+             ls='--', color='#c92508', label='Train loss')
+    ax1.plot(epoch_skip, val_loss_skip, linewidth=2,
+             color='#c92508', label='Validation loss')
+    ax1.spines['left'].set_color('#f23d1d')
+    # Coloring the ticks
+    for label in ax1.get_yticklabels():
+        label.set_color('#c92508')
+        label.set_size(12)
 
-    # Red line to indicate the optimal epoch (absolute value)
-    # can resize the font size of legend by modifying 'size'
+    # Plot valid/trian accuracy
+    ax2.plot(epoch_skip, tr_acc_skip, linewidth=2, ls='--',
+             color='#2348ff', label='Train Accuracy')
+    ax2.plot(epoch_skip, val_acc_skip, linewidth=2,
+             color='#2348ff', label='Validation Accuracy')
+    ax2.spines['right'].set_color('#2348ff')
+    # Coloring the ticks
+    for label in ax2.get_yticklabels():
+        label.set_color('#2348ff')
+        label.set_size(12)
+
+    # Manually setting the y-axis ticks
+    yticks = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    ax1.set_yticks(yticks)
+    ax2.set_yticks(yticks)
+
+    for label in ax1.get_xticklabels():
+        label.set_size(12)
+
+    # Modification of the overall graph
     fig.legend(ncol=4, loc=9, fontsize=12)
     plt.xlim(xmin=0)
-    ax2.set_ylim(ymax=0.98, ymin=0.77)
-    ax1.set_ylim(ymin=0.05, ymax=0.6)
-    # ax1.set_ylim(ymax=)
+    ax2.set_ylim(ymax=1, ymin=0)
+    ax1.set_ylim(ymax=1, ymin=0)
     plt.xlabel('epochs')
     plt.title("SGD optimizer", weight="bold")
     plt.grid(True, axis='y')
@@ -71,6 +89,6 @@ def plotloss(csvfile):
 
 
 if __name__ == '__main__':
-    file = '../history/SGD/history_SGD4.csv'
+    file = '../../history_RMS.csv'
     #file = '../history/SGD/history_SGD4.csv'
     plt.show(plotloss(file))
